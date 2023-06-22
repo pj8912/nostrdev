e036890c826787083f87d4add672183d78b9689e
John Pinto <59218902+pj8912@users.noreply.github.com>
2023-06-22 10:58:12 +0530

    Update app.js
    added copykeys function

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function generateKeys() {
                 divElement1.style.padding = '10px';
                 divElement1.style.wordBreak = 'break-all';
                 divElement1.style.margin = '2px';
-                divElement1.innerHTML = `Public Key: <span id="pwd_spn1" class="password-span">  ${response.public_key} </span> <button style="background:white;border:none;" id="cp_btn1" onclick="copy_password1()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-files" viewBox="0 0 16 16">
+                divElement1.innerHTML = `Public Key: <span id="pwd_spn1" class="password-span">  ${response.public_key} </span> <button onclick="copy_keys('pwd_spn1')" style="background:white;border:none;" id="cp_btn1" onclick="copy_password1()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-files" viewBox="0 0 16 16">
   <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z"/>
 </svg></button>`;
                 keysBox.appendChild(divElement1);
@@ -27,7 +27,7 @@ function generateKeys() {
                 divElement2.style.padding = '10px';
                 divElement2.style.wordBreak = 'break-all';
                 divElement2.style.margin = '2px';
-                divElement2.innerHTML = `Private Key: <span id="pwd_spn2" class="password-span"> ${response.private_key} </span> <button style="background:white;border:none;" id="cp_btn2" onclick="copy_password2()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-files" viewBox="0 0 16 16">
+                divElement2.innerHTML = `Private Key: <span id="pwd_spn2" class="password-span"> ${response.private_key} </span> <button onclick="copy_keys('pwd_spn1')" style="background:white;border:none;" id="cp_btn2" onclick="copy_password2()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-files" viewBox="0 0 16 16">
   <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z"/>
 </svg></button>`;
                 keysBox.appendChild(divElement2);
@@ -42,9 +42,10 @@ function generateKeys() {
 }
 
 
-function copy_password1() {
-    var copyText = document.getElementById("pwd_spn1");
-    var textArea = document.createElement("textarea");
+//copy generated keys
+function copy_keys(id){
+    var copyText = document.getElementById(id)
+    var textArea = document.createElement("textarea")
     textArea.value = copyText.textContent;
     document.body.appendChild(textArea);
     textArea.select();
@@ -52,16 +53,6 @@ function copy_password1() {
     textArea.remove();
 }
 
-
-function copy_password2() {
-    var copyText = document.getElementById("pwd_spn2");
-    var textArea = document.createElement("textarea");
-    textArea.value = copyText.textContent;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand("Copy");
-    textArea.remove();
-}
 
 function convertKeys() {
 
